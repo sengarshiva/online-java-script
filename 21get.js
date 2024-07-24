eb8a90256465864e3f684998367a230957235877
sengarshiva <shivasengar08@gmail.com>
2024-07-24 23:48:47 +0530

    first commit

---
+++ 21get.js
@@ -0,0 +1,10 @@
+// alert("hello")   //only check krne k liye ki ye link hui ya nhi
+
+
+
+let data=document.getElementById("a").innerText;
+//let data=document.getElementById("a").innerHTML;
+//let data=document.getElementById("a").getAttribute("style");
+//data=document.getElementById("a").getAttributeNode("style");
+//data=document.getElementById("a").attributes[0];
+console.log(data);
---
+++ 22set.js
@@ -0,0 +1,12 @@
+ document.getElementById("a").innerHTML = "<center><h2>PNINFOSY Gwalior 1234</h2></center>";
+ //document.getElementById("a").setAttribute("style","border:10px dotted red");
+//document.getElementById("a").removeAttribute("style");
+//document.getElementById("b").innerHTML = "<h1>GWALIOR</h1>";
+ 
+//querySelector
+// document.querySelector('#a').innerHTML = "<center><h2>PNINFOSY Gwalior 1234</h2></center>";
+//document.querySelector('.c').innerHTML="<h1>Hello Gwalior 1234566</h1>"
+
+
+
+//data do tarike se set kar skte hai ek document.getElementById se or dusra quearySelector ki help se.
